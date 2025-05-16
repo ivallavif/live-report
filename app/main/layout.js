851b71aee414b855ba79cab739b59ebb60888d41
ivallavif <Ivallavi.Fahrazi@asianet.co.id>
2025-05-16 11:28:12 +0700

    add guideline pdf

--- app/main/layout.js
+++ app/main/layout.js
@@ -9,7 +9,7 @@ import '@ant-design/v5-patch-for-react-19';
 import { Button } from "antd";
 import Swal from "sweetalert2";
 import { useRouter } from 'next/navigation'
-import {DownloadOutlined} from '@ant-design/icons'
+import { DownloadOutlined } from '@ant-design/icons'
 export default function MainLayout({ children }) {
     const router = useRouter()
 
@@ -36,7 +36,8 @@ export default function MainLayout({ children }) {
             <div className="flex justify-between items-center py-2 px-4 mb-2 border-b border-gray-200">
                 <img src="https://www.unifiber.id/assets/logos/logo-color.svg" className="w-24" />
                 <div className="flex gap-2 items-center">
-                    <Button href="/live_report_1.0.apk" icon={<DownloadOutlined/>}>APK</Button>
+                    <a href="/guideline.pdf" target="_blank" className="text-blue-400 font-semibold mx-4 cursor-pointer underline">Guidelines</a>
+                    <Button href="/live_report_1.0.apk" icon={<DownloadOutlined />}>APK</Button>
                     <Button onClick={signoutConfirmation}>Logout</Button>
                 </div>
             </div>
